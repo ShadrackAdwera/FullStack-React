223ff1c9f4a29c5ea4b758ce59520ed6a582a71b
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-26 05:24:15 +0300

    add the user information form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-import Head from 'next/head'
+import Card from '../components/Card';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <h1>Deez Nuts</h1>
-  )
+  return <Card>
+  <form className={styles.form}>
+      <div className={styles.control}>
+          <label htmlFor='name'>Your Name</label>
+          <input type='text' required id='name' />
+      </div>
+      <div className={styles.control}>
+          <label htmlFor='email'>Email</label>
+          <input type='email' required id='email' />
+      </div>
+      <div className={styles.control}>
+          <label htmlFor='feedback'>Give Feedback</label>
+          <textarea required id='feedback' rows={5}></textarea>
+      </div>
+      <div className={styles.actions}>
+          <button>Send Feedback</button>
+      </div>
+  </form>
+  </Card>;
 }
