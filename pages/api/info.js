ab359249ec7f284674cec567e6eee3eb945af659
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-26 05:52:51 +0300

    parse incoming request on the server

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,4 +1,14 @@
 const handler = (req,res) => {
+    if(req.method==='POST') {
+        const name = req.body.name;
+        const email = req.body.email;
+        const feedbacc = req.body.feedbacc;
+
+        const newFeedback = {
+            id: Date.now().toString(),
+            name, email, feedbacc
+        }
+    }
     return res.status(200).json({message: 'Hello World'})
 }
 
