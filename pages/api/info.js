2b6438f2b810bb4889416d259f86ccd33a293c8e
shadrack.adwera <adweshshaddie@gmail.com>
2021-03-26 06:04:49 +0300

    add server side code for post requests

--- pages/api/info.js
+++ pages/api/info.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path';
+
 const handler = (req,res) => {
     if(req.method==='POST') {
         const name = req.body.name;
@@ -8,6 +11,13 @@ const handler = (req,res) => {
             id: Date.now().toString(),
             name, email, feedbacc
         }
+        const filePath = path.join(process.cwd(), 'data', 'info.json');
+        //read data in file first the overwrite it using updated data. Use readfileAsync to block the method until operation is complete.
+        const fileInfo = fs.readFileSync(filePath);
+        const arrayFileData = JSON.parse(fileInfo);
+        arrayFileData.unshift(newFeedback);
+        await fs.writeFileSync(filePath, JSON.stringify(arrayFileData));
+        res.status(201).json({message: 'Your Information has been saved'});
     }
     return res.status(200).json({message: 'Hello World'})
 }
